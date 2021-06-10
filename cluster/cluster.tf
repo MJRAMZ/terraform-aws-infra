58dde03da45b7b68b9ffc6b5c4f5c7c74929dee1
MJRAMZ <34893606+MJRAMZ@users.noreply.github.com>
2021-06-10 13:52:04 -0700

    Add load balancer config

--- cluster/cluster.tf
+++ cluster/cluster.tf
@@ -27,7 +27,7 @@ data "aws_ami" "ubuntu_latest" {
   }
 }
 
-# Security group
+# Security group flor launch config
 resource "aws_security_group" "cluster" {
   name = "server_cluster-sg"
 
@@ -82,3 +82,84 @@ data "aws_subnet_ids" "default" {
   vpc_id = data.aws_vpc.default.id
 }
 
+# Load balancer
+resource "aws_lb" "cluster" {
+  name = "terraform-asg-cluster"
+  load_balancer_type = "application"
+  subnets = data.aws_subnet_ids.default.ids
+}
+
+# Load balancer listener
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.cluster.arn
+  port = 80
+  protocol = "HTTP"
+
+  # By default, return 404 page
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code = 404
+    }
+  }
+}
+
+# Load balancer target group
+resource "aws_lb_target_group" "asg" {
+  name = var.alb_name
+
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+# Load balancer listener rule
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+# Load balancer security group for LB
+resource "aws_security_group" "alb" {
+  name = var.alb_security_group_name
+
+  # Allow inbound HTTP requests
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound requests
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol ="-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
