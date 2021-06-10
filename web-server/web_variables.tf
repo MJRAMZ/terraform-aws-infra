d01c90087821841f6b718c4a8d19d53af21c61c1
MJRAMZ <34893606+MJRAMZ@users.noreply.github.com>
2021-06-10 11:26:20 -0700

    Add server port variable and outputs

--- web-server/web_variables.tf
+++ web-server/web_variables.tf
@@ -1,4 +1,4 @@
-# Variables for base configuration file
+# Variables for web-server configuration file
 variable "region" {
   type        = string
   description = "The AWS region"
@@ -9,4 +9,16 @@ variable "instance_type" {
   type        = string
   description = "The instance type to launch"
   default     = "t2.micro"
+}
+
+variable "server_port" {
+  type = number
+  description = "The port the server will use for HTTP requests"
+  default = 8080
+}
+
+# Outputs
+output "public_ip" {
+  value = aws_instance.web_server.public_ip
+  description = "The public IP address of the web server."
 }
